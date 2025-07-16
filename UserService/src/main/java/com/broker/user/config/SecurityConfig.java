29bd4591d10adf69a81dfb4cd07610fb9dda6842
Aarav <aaravgulati@drreddys.com>
2025-07-16 21:19:01 +0530

    Comments are back!

--- UserService/src/main/java/com/broker/user/config/SecurityConfig.java
+++ UserService/src/main/java/com/broker/user/config/SecurityConfig.java
@@ -54,16 +54,21 @@ public class SecurityConfig {
     }
 
 
+    //Note: Whenever we declare a class as bean, we tell spring to store the instance of the class returned
+    //      into spring container and use the same instance everywhere throughout the application
+
+    //      For example, we have two controllers, and in both controllers we Autowire PasswordEncoder object. Same
+    //      instance of PasswordEncoder will be used by both the controllers
 
 
     @Bean
     public AuthenticationManager authenticationManager(
             CustomUserDetailsService customUserDetailsService,
-            PasswordEncoder passwordEncoder) {
+            PasswordEncoder passwordEncoder) {  //Note that this is method injection. We have injected CustomUserDetailsService and PasswordEncoder into DaoAuthenticationProvider
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(passwordEncoder);
         authenticationProvider.setUserDetailsService(customUserDetailsService);
 
-        return new ProviderManager(authenticationProvider);
+        return new ProviderManager(authenticationProvider); //Note that AuthenticationManager is an interface and ProviderManager implements it. It also overrides the authenticate method of AuthenticationManager interface
     }
 
 }
--- UserService/src/main/java/com/broker/user/utils/JwtUtil.java
+++ UserService/src/main/java/com/broker/user/utils/JwtUtil.java
@@ -41,7 +41,7 @@ public class JwtUtil {
     }
 
     public String generateToken(String username) {
-        Map<String, Object> claims = new HashMap<>();
+        Map<String, Object> claims = new HashMap<>(); //We can pass any extra headers here
         return createToken(claims, username);
     }
 
